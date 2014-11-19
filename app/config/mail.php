721b8dd57ecaeff19583ab0f397fc39431e0b66e
Shihab <>
2014-11-20 00:22:07 +0600

    Shihab: demo mail

--- app/config/mail.php
+++ app/config/mail.php
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'developersdextrous@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => 'Ebitgroup2',
 
 	/*
 	|--------------------------------------------------------------------------
---
+++ app/views/emails/new_mail.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h3>Hello {{ $user->first_name }}!!</h3>
+        <p>Welcome to Xero!! This a demo! Enjoy!</p>
+    </body>
+</html>
